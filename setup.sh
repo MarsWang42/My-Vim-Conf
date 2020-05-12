d360b0247fa527f86c5625f305dc3981f93af201
Mars <mars.w42@gmail.com>
2020-05-11 18:35:31 -0700

    Added rust and vim support to setup script.

--- setup.sh
+++ setup.sh
@@ -21,6 +21,10 @@ sudo apt-get install -y python3-setuptools
 sudo easy_install3 pip
 
 
+# Install Rust
+curl https://sh.rustup.rs -sSf | sh
+
+
 # Install Node.js
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
 sudo bash n lts
@@ -41,13 +45,16 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -y neovim
 
+
+# Install vim setup dependencies
 python2 -m pip install --user --upgrade pynvim
 python3 -m pip install --user --upgrade pynvim
-
 npm install -g neovim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cargo install bat
+cargo install devicon-lookup
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
